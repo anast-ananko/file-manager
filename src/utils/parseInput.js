cc47057e79fe76399fb68137f7ffc8542d387b73
Nastassia Ananka <anast.ananko@gmail.com>
2023-06-24 23:05:48 +0200

    feat: handling the situation of having multiple spaces

--- src/utils/parseInput.js
+++ src/utils/parseInput.js
@@ -1,3 +1,5 @@
-export const parseInput = (arr) => {
-  return arr.split(' ');
+export const parseInput = (str) => {
+  console.log(typeof arr);
+  const newStr = str.replace(/\s+/g, ' ');
+  return newStr.split(' ');
 }
